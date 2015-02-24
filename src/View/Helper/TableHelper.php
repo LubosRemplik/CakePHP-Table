f1f4e49d96a82646a330e0392583a3be36d5b99c
Lubos Remplik <lubos@on-idle.com>
2015-02-24 17:26:47 +0100

    Basic table with possible to display head, body and foot tags

--- src/View/Helper/TableHelper.php
+++ src/View/Helper/TableHelper.php
@@ -2,10 +2,13 @@
 namespace Lubos\Table\View\Helper;
 
 use Cake\View\Helper;
+use Cake\View\StringTemplateTrait;
 use Cake\View\View;
 
 class TableHelper extends Helper
 {
+
+    use StringTemplateTrait;
 
     /**
      * Other helpers used by FormHelper
@@ -13,6 +16,59 @@ class TableHelper extends Helper
      * @var array
      */
     public $helpers = ['Url', 'Html', 'Form'];
+
+    /**
+     * Table body content
+     *
+     * @var array
+     */
+    protected $_body = array();
+
+    /**
+     * Table head content
+     *
+     * @var array
+     */
+    public $_head = array();
+
+    /**
+     * Table foot content
+     *
+     * @var array
+     */
+    protected $_foot = array();
+
+    /**
+     * Last group used
+     *
+     * @var string
+     */
+    protected $_lastGroup;
+
+    /**
+     * Default config for this class
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'templates' => [
+            'tableheader' => '<th{{attrs}}>{{content}}</th>',
+            'tablecell' => '<td{{attrs}}>{{content}}</td>',
+            'tablerow' => '<tr{{attrs}}>{{content}}</tr>',
+            'tablerowstart' => '<tr{{attrs}}>',
+            'tablerowend' => '</tr>',
+            'tablehead' => '<thead{{attrs}}>{{content}}</thead>',
+            'tableheadstart' => '<thead{{attrs}}>',
+            'tableheadend' => '</thead>',
+            'tablebody' => '<tbody{{attrs}}>{{content}}</tbody>',
+            'tablebodystart' => '<tbody{{attrs}}>',
+            'tablebodyend' => '</tbody>',
+            'tablefoot' => '<tfoot{{attrs}}>{{content}}</tfoot>',
+            'tablefootstart' => '<tfoot{{attrs}}>',
+            'tablefootend' => '</tfoot>',
+            'table' => '<table{{attrs}}>{{content}}</table>',
+        ]
+    ];
 
     /**
      * Constructor
@@ -23,8 +79,88 @@ class TableHelper extends Helper
     public function __construct(View $View, array $config = [])
     {
         parent::__construct($View, $config);
+    }
 
-        $config = $this->_config;
+    /**
+     * Create table
+     *
+     * @param array $options Array of options.
+     * @return $this
+     */
+    public function create(array $options = array())
+    {
+        // empty body, head and foot
+        $this->_body = $this->_head = $this->_foot = '';
+        return $this;
+    }
+
+    /**
+     * Start table row
+     *
+     * ### Options
+     *
+     * - `group` Table group options head, body, foot. Default body.
+     *
+     * @param array $options Array of options and html attributes.
+     * @return $this
+     */
+    public function startRow(array $options = array())
+    {
+        $group = 'body';
+        if (isset($options['group'])) {
+            $group = $options['group'];
+            unset($options['group']);
+        }
+        if (isset($this->{'_' . $group})) {
+            $this->{'_' . $group}[] = $this->formatTemplate('tablerowstart', [
+                'attrs' => $this->templater()->formatAttributes($options),
+            ]);
+        }
+        $this->_lastGroup = $group;
+        return $this;
+    }
+
+    /**
+     * Table header
+     *
+     * @param string $content Header cell content.
+     * @param array $htmlAttributes Html attributes passed to th.
+     * @return $this
+     */
+    public function header($content = null, array $htmlAttributes = array())
+    {
+        $this->_head[] = $this->formatTemplate('tableheader', [
+            'content' => $content,
+            'attrs' => $this->templater()->formatAttributes($htmlAttributes),
+        ]);
+        return $this;
+    }
+
+    /**
+     * Table cell
+     *
+     * @param string $content Cell content.
+     * @param array $htmlAttributes Html attributes passed to td.
+     * @return $this
+     */
+    public function cell($content = null, array $htmlAttributes = array())
+    {
+        $this->{'_' . $this->_lastGroup}[] = $this->formatTemplate('tablecell', [
+            'content' => $content,
+            'attrs' => $this->templater()->formatAttributes($htmlAttributes),
+        ]);
+        return $this;
+    }
+
+    /**
+     * End table row
+     *
+     * @return $this
+     */
+    public function endRow()
+    {
+        $this->{'_' . $this->_lastGroup}[] = $this->formatTemplate('tablerowend', []);
+        return $this;
     }
 
     /**
@@ -33,13 +169,28 @@ class TableHelper extends Helper
      */
     public function display(array $htmlAttributes = [])
     {
-        $output = implode([
-            $this->Html->tag('tr', implode([
-                $this->Html->tag('td', 'Whatever'),
-                $this->Html->tag('td', 'Test')
-            ]))
+        $out = [];
+        if ($this->_head) {
+            $out[] = $this->formatTemplate('tablehead', [
+                'content' => implode($this->_head)
+            ]);
+        }
+        if ($this->_body && (empty($this->_head) || empty($this->_foot))) {
+            $out[] = implode($this->_body);
+        } elseif ($this->_body) {
+            $out[] = $this->formatTemplate('tablebody', [
+                'content' => implode($this->_body)
+            ]);
+        }
+        if ($this->_foot) {
+            $out[] = $this->formatTemplate('tablefoot', [
+                'content' => implode($this->_foot)
+            ]);
+        }
+        $out = $this->formatTemplate('table', [
+            'attrs' => $this->templater()->formatAttributes($htmlAttributes),
+            'content' => implode($out)
         ]);
-        $output = $this->Html->tag('table', $output, $htmlAttributes);
-        return $output;
+        return $out;
     }
 }
--- tests/TestCase/View/Helper/TableHelperTest.php
+++ tests/TestCase/View/Helper/TableHelperTest.php
@@ -1,7 +1,6 @@
 <?php
 namespace Lubos\Table\Test\TestCase\View\Helper;
 
-use Cake\Controller;
 use Cake\TestSuite\TestCase;
 use Cake\View\View;
 use Lubos\Table\View\Helper\TableHelper;
@@ -31,14 +30,42 @@ class TableHelperTest extends TestCase
     }
 
     /**
-     * testDisplay method
-     *
-     * @return void
+     * testStartRow method
      */
     public function testDisplay()
     {
+        $View = new View(null);
+
+        // simple table
+        $expected = '<table>';
+        $expected .= '<tr><td>cell</td></tr>';
+        $expected .= '</table>';
+        $this->Table
+            ->create()
+            ->startRow()
+            ->cell('cell')
+            ->endRow();
         $result = $this->Table->display();
-        $expected = '<table><tr><td>Whatever</td><td>Test</td></tr></table>';
-        $this->assertEquals($result, $expected);
+        $this->assertEquals($expected, $result);
+
+        // table with class for row, thead group, tbody, tfoot
+        $expected = '<table>';
+        $expected .= '<thead><tr class="header"><th>header</th></tr></thead>';
+        $expected .= '<tbody><tr><td>cell</td></tr></tbody>';
+        $expected .= '<tfoot><tr><td>footer</td></tr></tfoot>';
+        $expected .= '</table>';
+        $this->Table
+            ->create()
+            ->startRow(['group' => 'head', 'class' => 'header'])
+            ->header('header')
+            ->endRow()
+            ->startRow()
+            ->cell('cell')
+            ->endRow()
+            ->startRow(['group' => 'foot'])
+            ->cell('footer')
+            ->endRow();
+        $result = $this->Table->display();
+        $this->assertEquals($expected, $result);
     }
 }
